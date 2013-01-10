1844aa08fd1c9f4a3958d3c1eed8957df585f4c5
toshi <me@toshimaru.net>
2013-01-10 14:19:46 +0900

    add oh-my-zsh install script.

---
+++ tools/oh_my_zsh_install.sh
@@ -0,0 +1,7 @@
+# refs. https://github.com/robbyrussell/oh-my-zsh
+
+# via `curl`
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# via `wget`
+# wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
