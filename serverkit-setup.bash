8d039c0de3352fbc41c3e3b7a2ad0bd4487dc90a
toshi <me@toshimaru.net>
2016-04-01 15:05:40 +0900

    Add home-brew install phase

--- serverkit-setup.bash
+++ serverkit-setup.bash
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Install homebrew
+which brew > /dev/null
+if [ "$?" -ne 0 ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 bundle exec serverkit apply recipe.yml.erb --variables=variables.yml
