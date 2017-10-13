840ef7322ad1774a04aec11b2743b38c2e922a97
toshimaru <me@toshimaru.net>
2017-10-13 13:34:38 +0900

    Add `bundle install`

--- serverkit-setup.bash
+++ serverkit-setup.bash
@@ -6,4 +6,5 @@ if [ "$?" -ne 0 ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+bundle install --path vendor/bundle
 bundle exec serverkit apply recipe.yml.erb --variables=variables.yml
