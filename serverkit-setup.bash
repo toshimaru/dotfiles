ca10cdc96a35741daf6efb06c3555eeae8f81498
toshimaru <me@toshimaru.net>
2022-06-12 10:28:31 +0900

    Run serverkit-setup.bash

--- serverkit-setup.bash
+++ serverkit-setup.bash
@@ -6,5 +6,5 @@ if [ "$?" -ne 0 ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-bundle install --path vendor/bundle
+bundle install --path vendor/bundle --jobs 4 --retry 3
 bundle exec serverkit apply serverkit.yml.erb --variables=serverkit-variables.yml
