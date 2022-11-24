c5d3adeae645b55faaf35a62d575d75f490d412c
Toshimaru <me@toshimaru.net>
2022-11-24 20:59:41 +0900

    Fix brew installation

--- serverkit-setup.bash
+++ serverkit-setup.bash
@@ -1,12 +1,12 @@
 #!/bin/bash -eu
 
-# Install homebrew
-which brew > /dev/null
-if [ "$?" -ne 0 ]; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install homebrew if it's not installed
+if ! which brew > /dev/null ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Use Ruby 2.7.x
+# TODO: Bump Ruby version
 brew update && brew bundle
 eval "$(rbenv init -)"
 rbenv install 2.7.6 && rbenv local 2.7.6
